18e1b762de92ed2b7042b67cf10ab514f6e06a2c
AV912 <88800484+AV912@users.noreply.github.com>
2023-08-07 14:33:39 -0400

    Added spring starter backend code

---
+++ backend_spring/hustle/src/main/java/com/hustle/hustle/HustleApplication.java
@@ -0,0 +1,13 @@
+package com.hustle.hustle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustleApplication.class, args);
+	}
+
+}
---
+++ backend_spring/hustle/src/test/java/com/hustle/hustle/HustleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hustle.hustle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
